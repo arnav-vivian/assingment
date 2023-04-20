7b68b52d7d163734ee338e4b2fc8b38aeafe8b61
arnav singh <coolarnav932@gmail.com>
2023-04-21 03:23:29 +0530

    2nd commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,78 +1,87 @@
 import React from "react";
 import { useState } from "react";
+import Modal from "./Modal";
+
+import Fetch from "../hooks/Fetch";
 
 export default function CardComponent() {
-    const [pie, setPie] = useState(false);
-    const posts = [
-        {
-            title: "Grid 1",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: " Grid 2",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: " Grid 3",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: "Grid 3",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: " Grid 2",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: " Grid 1",
-            img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-    ];
 
-    const openPie = () => {
-        setPie(!pie);
-        console.log(pie);
-        if (setPie === true) {
-            <div>hi</div>
-        }
+    const { data, loading, error } = Fetch(`https://fakestoreapi.com/products`);
+    // console.log(data);
+    // const posts = [
+    //     {
+    //         title: "Grid 1",
+    //         img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    //     {
+    //         title: " Grid 2",
+    //         img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    //     {
+    //         title: " Grid 3",
+    //         img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    //     {
+    //         title: "Grid 3",
+    //         img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    //     {
+    //         title: " Grid 2",
+    //     img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    //     {
+    //         title: " Grid 1",
+    //         img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+    //         content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
+    //     },
+    // ];
+
+    const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleClick = () => {
+        setOpenModal(!openModal);
+        //console.log(openModal);
+
+
+
     }
     return (
         <>
-            <div className="grid gap-28 mt-9 m-10 lg:grid-cols-3  mx-10 ">
-                {posts.map((items, key) => (
+            <div className="grid gap-28 mt-9 m-10 lg:grid-cols-3  mx-10">
+                {data.map((items, key) => (
                     <div className="w-full rounded-2xl shadow-2xl lg:max-w-sm p-8" key={key}>
-                        <img className="object-cover w-full h-48 rounded-2xl" src={items.img} alt="image" />
-                        <div className="flex justify-end -mt-12 px-2">
-                            <button class="bg-transparent hover:bg-blue-500 text-black font-semibold hover:text-white py-1 px-2 border border-black hover:border-transparent rounded">Catogery</button>
+                        <img className=" max-h-48 min-h-fit object-contain w-full h-full rounded-2xl" src={items.image} alt="image" />
+                        <div className="flex justify-end -mt-4 px-2">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                                {items.category}
+                            </button>
                         </div>
                         <div className="p-4">
-                            <h4 className="text-xl font-semibold text-blue-600">
+                            <h4 className="text-sm font-semibold text-blue-600 py-4">
 
                                 {items.title}
                             </h4>
-                            <p className="mb-2 leading-normal">
-                                {items.content}
+                            <p className="mb-2 leading-normal text-black text-xs">
+                                {items.description}
                             </p>
-                            <button className="px-4 py-2 text-sm text-blue-100 rounded-2xl bg-blue-500 ">
-                                Read more
-                            </button>
+
                         </div>
                     </div>
                 ))}
 
             </div>
             <div className=" sticky bottom-6  flex justify-end px-4">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={openPie}>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={handleClick}>
                     Button
                 </button>
             </div >
+            {openModal && <Modal setOpen={setOpenModal} />}
 
         </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Cards from '../components/Cards'
+import Cards2 from '../components/Cards2'
 const Home = () => {
     return (
         <>
